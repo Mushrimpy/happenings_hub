b8e868b6046482d59f0d7a055648dd261ca3545d
Moncef Slimani <moncef.slimani.ms@gmail.com>
2024-07-13 19:44:05 +0100

    basic template

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,53 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash
+import os
+
+load_dotenv()
+db = SQLAlchemy()
+DB_NAME = os.getenv('DB_NAME')
 
 def create_app():
-    app = Flask(__name__)    
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=os.getenv('SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI=f'sqlite:///{DB_NAME}',
+        UPLOAD_FOLDER=os.path.join(app.instance_path, os.getenv('UPLOAD_FOLDER')),
+    )
+    db.init_app(app)
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+    from .views import views_bp
+    from .auth import auth_bp
+    
+    app.register_blueprint(views_bp, url_prefix='/')
+    app.register_blueprint(auth_bp, url_prefix='/auth/')
+    
+    from .errors import page_not_found
+    app.register_error_handler(404, page_not_found)
+    
+    from .models import User
+    create_database(app)
+    
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth_bp.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return app
 
-            
+def create_database(app):
+    from .models import User
+    db_path = os.path.join(app.instance_path, DB_NAME)
+    if not os.path.exists(db_path):
+        with app.app_context():
+            db.create_all()
+            print('Database created')
+            admin_user = User(username='admin', password=generate_password_hash(os.getenv('ADMIN_PW')))
+            db.session.add(admin_user)
+            db.session.commit()
---
+++ website/auth.py
@@ -0,0 +1,58 @@
+from flask import Blueprint, request, flash, redirect, flash, url_for, render_template
+from werkzeug.security import check_password_hash, generate_password_hash
+from .models import User
+from flask_login import login_user, logout_user, current_user, login_required
+from . import db
+
+
+auth_bp = Blueprint('auth_bp', __name__)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views_bp.home'))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('Username does not exist', category='error')
+
+    return render_template('login.html', user=current_user)
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Logged out", category='success')
+    return redirect(url_for('auth_bp.login'))
+
+@auth_bp.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+          
+        user = User.query.filter_by(username=username).first()
+        
+        if user:
+            flash('Username already exists', category='error')
+        else:
+            if password1 != password2:
+                flash('Passwords don\'t match', category='error')                
+            else:
+                new_user = User(username=username, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                flash('Account created', category='success')
+                return redirect(url_for('views_bp.home'))
+            
+    return render_template("sign_up.html", user=current_user)
