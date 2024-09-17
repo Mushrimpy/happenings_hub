76c82b5ba089fc30e93fed85eac6d48a5b9700c9
Moncef Slimani <moncef.slimani.ms@gmail.com>
2024-09-17 21:17:14 +0100

    initial commit to migrate codebase

---
+++ README.md
@@ -0,0 +1,1 @@
+# Happenings Hub
--- app/errors.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from flask_login import current_user
-
-
-def page_not_found(error):
-    return render_template("404.html", user=current_user), 404
